2362c2f0d2583e1c684a2b361fd2da40e160c09a
Kazuyoshi Tomita <kztomita@gmail.com>
2022-06-10 21:44:01 +0900

    route追加

--- Controller/ProductController.php
+++ Controller/ProductController.php
@@ -4,6 +4,7 @@ namespace Plugin\MultiLingual\Controller;
 
 use Eccube\Controller\AbstractController;
 use Eccube\Entity\Product;
+use Knp\Component\Pager\PaginatorInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
@@ -11,7 +12,18 @@ use Symfony\Component\HttpFoundation\Request;
 class ProductController extends AbstractController
 {
     /**
-     *
+     * @Route("/{_locale}/products/list", name="product_list_en", methods={"GET"}, requirements={"_locale": "en|cn"})
+     * @Template("Product/list.twig")
+     */
+    public function index(Request $request, PaginatorInterface $paginator)
+    {
+        return $this->forward('Eccube\Controller\ProductController::index', [
+            'request'  => $request,
+            'paginator' => $paginator,
+        ]);
+    }
+
+    /**
      * @Route("/{_locale}/products/detail/{id}", name="product_detail_locale", methods={"GET"}, requirements={"id" = "\d+", "_locale": "en|cn",})
      * @Template("Product/detail.twig")
      */
