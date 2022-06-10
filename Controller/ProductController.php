46129d8e09b49016204fc69f12e1ce98236cbff2
Kazuyoshi Tomita <kztomita@gmail.com>
2022-06-10 21:37:48 +0900

    継承を使わないように修正

--- Controller/ProductController.php
+++ Controller/ProductController.php
@@ -2,21 +2,25 @@
 
 namespace Plugin\MultiLingual\Controller;
 
+use Eccube\Controller\AbstractController;
 use Eccube\Entity\Product;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
 
-// TODO compositionにできないか
-class ProductController extends \Eccube\Controller\ProductController
+class ProductController extends AbstractController
 {
     /**
      *
      * @Route("/{_locale}/products/detail/{id}", name="product_detail_locale", methods={"GET"}, requirements={"id" = "\d+", "_locale": "en|cn",})
      * @Template("Product/detail.twig")
      */
-    public function detail(Request $request, Product $product)
+    public function detail(Request $request, Product $Product)
     {
-        return parent::detail($request, $product);
+        // ref. to https://symfony.com/doc/current/controller/forwarding.html
+        return $this->forward('Eccube\Controller\ProductController::detail', [
+            'request'  => $request,
+            'Product' => $Product,
+        ]);
     }
 }
