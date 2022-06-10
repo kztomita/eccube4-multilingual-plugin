b8093f045df2af7bd803c76aebdd450428e1d9f2
Kazuyoshi Tomita <kztomita@gmail.com>
2022-06-11 00:59:43 +0900

    テンプレートの切り替え

--- Controller/ProductController.php
+++ Controller/ProductController.php
@@ -9,18 +9,33 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
 
+/*
+ * コントローラはオリジナルのものを利用する。
+ * forward()すると転送先のactionでannotationしたテンプレートが使われる。
+ * テンプレートを分けたい場合は、コントローラを生成して手動で呼び出す。
+ *
+ * https://stackoverflow.com/questions/17611447/forward-with-another-template
+ */
+
 class ProductController extends AbstractController
 {
     /**
+     * @var \Eccube\Controller\ProductController
+     */
+    private $controller;
+
+    public function __construct(\Eccube\Controller\ProductController $c)
+    {
+        $this->controller = $c;
+    }
+
+    /**
      * @Route("/{_locale}/products/list", name="product_list_en", methods={"GET"}, requirements={"_locale": "en|cn"})
-     * @Template("Product/list.twig")
+     * @Template("@MultiLingual/Product/list.twig")
      */
     public function index(Request $request, PaginatorInterface $paginator)
     {
-        return $this->forward('Eccube\Controller\ProductController::index', [
-            'request'  => $request,
-            'paginator' => $paginator,
-        ]);
+        return $this->controller->index($request, $paginator);
     }
 
     /**
