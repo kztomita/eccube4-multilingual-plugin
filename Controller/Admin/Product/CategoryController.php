f873443095f4b16f7f2ac1a8e58f84c11522035f
Kazuyoshi Tomita <kztomita@gmail.com>
2022-06-14 20:03:02 +0900

    カテゴリ管理のテンプレートを差し替え

---
+++ Controller/Admin/Product/CategoryController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Plugin\MultiLingual\Controller\Admin\Product;
+
+use Eccube\Controller\AbstractController;
+use Eccube\Util\CacheUtil;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+
+class CategoryController extends AbstractController
+{
+    /**
+     * @var \Eccube\Controller\Admin\Product\CategoryController
+     */
+    private $controller;
+
+    public function __construct(
+        \Eccube\Controller\Admin\Product\CategoryController $c
+    )
+    {
+        $this->controller = $c;
+    }
+
+    /**
+     * デフォルトのカテゴリ管理のコントローラのテンプレートを差し替える。
+     *
+     * @Route("/%eccube_admin_route%/product/category", name="admin_product_category", methods={"GET", "POST"})
+     * @Route("/%eccube_admin_route%/product/category/{parent_id}", requirements={"parent_id" = "\d+"}, name="admin_product_category_show", methods={"GET", "POST"})
+     * @Route("/%eccube_admin_route%/product/category/{id}/edit", requirements={"id" = "\d+"}, name="admin_product_category_edit", methods={"GET", "POST"})
+     * @Template("MultiLingual/Resource/template/admin/Product/category.twig")
+     */
+    public function index(Request $request, $parent_id = null, $id = null, CacheUtil $cacheUtil)
+    {
+        $this->controller->setContainer($this->container);
+
+        return $this->controller->index($request, $parent_id, $id, $cacheUtil);
+    }
+}
+
