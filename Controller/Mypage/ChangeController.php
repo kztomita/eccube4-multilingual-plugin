e39c1874fe2f36b1c1071d39808613f3aa9f9762
Kazuyoshi Tomita <kztomita@gmail.com>
2022-06-25 12:12:22 +0900

    ログインチェックを追加

--- Controller/Mypage/ChangeController.php
+++ Controller/Mypage/ChangeController.php
@@ -6,6 +6,7 @@ use Eccube\Controller\AbstractController;
 use Plugin\MultiLingual\Controller\LocaleTrait;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -28,6 +29,7 @@ class ChangeController extends AbstractController
     /**
      * @Route("/{_locale}/mypage/change", name="mypage_change_locale")
      * @Template("MultiLingual/Resource/template/default/Mypage/change.twig")
+     * @Security("is_granted('IS_AUTHENTICATED_FULLY')")
      */
     public function index(Request $request)
     {
@@ -45,6 +47,7 @@ class ChangeController extends AbstractController
     /**
      * @Route("/{_locale}/mypage/change_complete", name="mypage_change_complete_locale")
      * @Template("MultiLingual/Resource/template/default/Mypage/change_complete.twig")
+     * @Security("is_granted('IS_AUTHENTICATED_FULLY')")
      */
     public function complete(Request $request)
     {
--- Controller/Mypage/MypageController.php
+++ Controller/Mypage/MypageController.php
@@ -9,6 +9,7 @@ use Knp\Component\Pager\Paginator;
 use Knp\Component\Pager\PaginatorInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -31,6 +32,7 @@ class MypageController extends AbstractController
     /**
      * @Route("/{_locale}/mypage/login", name="mypage_login_locale")
      * @Template("MultiLingual/Resource/template/default/Mypage/login.twig")
+     * @Security("is_granted('IS_AUTHENTICATED_ANONYMOUSLY')")
      */
     public function login(Request $request, AuthenticationUtils $utils)
     {
@@ -44,6 +46,7 @@ class MypageController extends AbstractController
     /**
      * @Route("/{_locale}/mypage/", name="mypage_locale")
      * @Template("MultiLingual/Resource/template/default/Mypage/index.twig")
+     * @Security("is_granted('ROLE_USER')")
      */
     public function index(Request $request, Paginator $paginator)
     {
@@ -57,6 +60,7 @@ class MypageController extends AbstractController
     /**
      * @Route("/{_locale}/history/{order_no}", name="mypage_history_locale")
      * @Template("MultiLingual/Resource/template/default/Mypage/history.twig")
+     * @Security("is_granted('ROLE_USER')")
      */
     public function history(Request $request, $order_no)
     {
@@ -69,6 +73,7 @@ class MypageController extends AbstractController
 
     /**
      * @Route("/{_locale}/mypage/order/{order_no}", name="mypage_order_locale", methods={"PUT"})
+     * @Security("is_granted('ROLE_USER')")
      */
     public function order(Request $request, $order_no)
     {
@@ -83,6 +88,7 @@ class MypageController extends AbstractController
     /**
      * @Route("/{_locale}/mypage/favorite", name="mypage_favorite_locale")
      * @Template("MultiLingual/Resource/template/default/Mypage/favorite.twig")
+     * @Security("is_granted('ROLE_USER')")
      */
     public function favorite(Request $request, Paginator $paginator)
     {
@@ -95,6 +101,7 @@ class MypageController extends AbstractController
 
     /**
      * @Route("/{_locale}/mypage/favorite/{id}/delete", name="mypage_favorite_delete_locale", methods={"DELETE"}, requirements={"id" = "\d+"})
+     * @Security("is_granted('ROLE_USER')")
      */
     public function delete(Request $request, Product $Product)
     {
