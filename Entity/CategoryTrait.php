293eed0c9eaff51fcd5546e95ef5b6d37d8d70db
Kazuyoshi Tomita <kztomita@gmail.com>
2022-06-27 11:09:07 +0900

    検索フォームのカテゴリプルダウンの多言語対応

--- Entity/CategoryTrait.php
+++ Entity/CategoryTrait.php
@@ -4,8 +4,10 @@ namespace Plugin\MultiLingual\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\Criteria;
 use Doctrine\ORM\Mapping as ORM;
 use Eccube\Annotation\EntityExtension;
+use Plugin\MultiLingual\Common\LocaleHelper;
 
 /**
  * @EntityExtension("Eccube\Entity\Category")
@@ -38,5 +40,23 @@ trait CategoryTrait
     public static function getLocaleClass(): string
     {
         return LocaleCategory::class;
+    }
+
+    public function getLocaleName(): string
+    {
+        $criteria = Criteria::create();
+        $criteria->where(Criteria::expr()->eq('locale', LocaleHelper::getCurrentRequestLocale()));
+
+        $locales = $this->getLocales()->matching($criteria);
+        if ($locales->count() == 0) {
+            return $this->getName();
+        } else {
+            return $locales[0]->getName();
+        }
+    }
+
+    public function getLocaleNameWithLevel(): string
+    {
+        return str_repeat('　', $this->getHierarchy() - 1).$this->getLocaleName();
     }
 }
---
+++ Form/Extension/SearchProductBlockExtension.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Plugin\MultiLingual\Form\Extension;
+
+use Eccube\Form\Type\SearchProductBlockType;
+use Eccube\Repository\CategoryRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * SearchProductBlockTypeを拡張する
+ */
+class SearchProductBlockExtension extends AbstractTypeExtension
+{
+    /**
+     * @var CategoryRepository
+     */
+    protected $categoryRepository;
+
+    public function __construct(CategoryRepository $categoryRepository)
+    {
+        $this->categoryRepository = $categoryRepository;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $Categories = $this->categoryRepository
+            ->getList(null, true);
+
+        // category_id項目を上書き。
+        // getLocaleNameWithLevel()でラベルを取得するようにする。
+        $builder->add('category_id', EntityType::class, [
+            'class' => 'Eccube\Entity\Category',
+            'choice_label' => 'LocaleNameWithLevel',
+            'choices' => $Categories,
+            'placeholder' => 'common.select__all_products',
+            'required' => false,
+        ]);
+    }
+
+    public function getExtendedType()
+    {
+        return SearchProductBlockType::class;
+    }
+
+    public static function getExtendedTypes(): iterable
+    {
+        yield SearchProductBlockType::class;
+    }
+
+}
+
