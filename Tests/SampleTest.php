20987db07c943e4436c121540cd7a61c7c2899b8
Kazuyoshi Tomita <kztomita@gmail.com>
2022-06-30 11:13:22 +0900

    unittestのサンプルを追加

---
+++ Tests/SampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Plugin\MultiLingual\Tests;
+
+use Eccube\Tests\EccubeTestCase;
+
+class SampleTest extends EccubeTestCase
+{
+    public function testDummy()
+    {
+        $this->assertTrue(true);
+    }
+}
