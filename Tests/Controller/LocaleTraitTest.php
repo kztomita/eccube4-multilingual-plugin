2ea9a33db04480ddc976ed7f6a9652167d7da9bd
Kazuyoshi Tomita <kztomita@gmail.com>
2022-06-30 15:00:30 +0900

    テスト用にrouteを追加

--- Tests/Controller/LocaleTraitTest.php
+++ Tests/Controller/LocaleTraitTest.php
@@ -5,8 +5,11 @@ namespace Plugin\MultiLingual\Tests\Controller;
 use Eccube\Controller\AbstractController;
 use Eccube\Tests\EccubeTestCase;
 use Plugin\MultiLingual\Controller\LocaleTrait;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\Router;
 
 class BaseController extends AbstractController
 {
@@ -45,17 +48,29 @@ class LocaleController extends AbstractController
 
 class LocaleTraitTest extends EccubeTestCase
 {
+    private function dumpRoutes(Router $router)
+    {
+        $collection = $router->getRouteCollection();
+        foreach ($collection->all() as $routeId => $route) {
+            printf("%s %s\n", $routeId, $route->getPath());
+        }
+    }
+
     public function testRedirect()
     {
         #self::bootKernel();
         $container = self::$kernel->getContainer();
 
-        /// pluginを有効にしておかないといけない？
-        $router = $container->get('router');
-        $route = $router->match('/shopping')['_route'];
-        //$route = $router->match('/en/shopping')['_route'];
+        $route = new Route('/{_locale}/shopping');
+        $container->get('router')
+                  ->getRouteCollection()
+                  ->add('shopping_locale', $route);
+
+        /*
+        $this->dumpRoutes($container->get('router'));
+        $route = $container->get('router')->match('/en/shopping')['_route'];
         print $route;
-        ///
+        */
 
         $request = new Request;
         $request->setLocale('en');
@@ -63,10 +78,10 @@ class LocaleTraitTest extends EccubeTestCase
         $localeController = new LocaleController(new BaseController);
         $localeController->setContainer($container);
 
+
         $response = $localeController->redirectToShopping($request);
 
         $this->assertTrue($response instanceof RedirectResponse);
-
         $this->assertEquals('/en/shopping', $response->getTargetUrl());
     }
 }
